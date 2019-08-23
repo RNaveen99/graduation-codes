c93f882e1c607a334bd5329f5bea96e81a55f5f7
Naveen <naveenrohilla99@gmail.com>
2019-08-23 11:37:20 +0530

    Added System Programming codes

---
+++ System Programming/ipConfig/myScanner.h
@@ -0,0 +1,4 @@
+#define Link 1
+#define IPv4 2
+#define Subnet 3
+#define Default 4
---
+++ System Programming/number of comments/comments.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    abc 
+    */
+    /* int b; () & % < > = " // */
+    // int a;
+// //fdr/*  */
+/* abcd 
+efgh
+ *****/ 
+ /* */
+ /* */ /* */
+    string a = "// //";
+    string b = "//";
+    string c = "/* */";
+    string d = "/* // */";
+    string e = "// /* */";
+    string f = "// /* ";
+    string g = "heloo\"";
+    string h = "heloo\";";
+    string i = "heloo\";/*  */";
+    /* ***/
+    /* **** \* */
+    cin >> a;
+    if (a & 1) {
+        cout << "Odd Number";
+    } else {
+        cout << "Event Number";
+    }
+    /*  /* */ 
+    cout << "Comment ends";
+    /*
+    if (a & 1) {
+        // if (a % 2 == 1)
+        cout << "Odd Number";
+    } else {
+        cout << "Event Number";
+    }
+    */
+//    return 1; /* return value */
+    return 0;
+}
